7e60c70ffe5acb86832e9a199ec479c1a3d9b204
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-06-30 17:36:30 -0600

    Redux Toolkit - thunk basic setup without async fetch

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getPokemons } from './store'
+
 export const PokemonApp = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPokemons())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <>
       <h1>Pokemon App</h1>
