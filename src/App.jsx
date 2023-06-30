449bcb7a852bbb446c676b624820dc1d75e45757
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-06-30 13:40:26 -0600

    Redux Toolkit - accesing and modifying the store

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  decrement,
+  increment,
+  incrementBy,
+} from './store/slices/counter/counterSlice'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { counter } = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>count is {counter}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          onClick={() => {
+            dispatch(increment())
+          }}
+        >
+          Increment
+        </button>
+      </div>
+
+      <div className="card">
+        <button
+          onClick={() => {
+            dispatch(decrement())
+          }}
+        >
+          Decrement
+        </button>
+      </div>
+
+      <div className="card">
+        <button
+          onClick={() => {
+            dispatch(incrementBy(2))
+          }}
+        >
+          Increment by 2
         </button>
       </div>
     </>
