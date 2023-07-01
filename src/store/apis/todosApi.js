e16daabfd3645aeb547bb70ea96790bb368bd4f4
Marcel Cabrera <marcel.cabrera@gmail.com>
2023-07-01 00:02:09 -0600

    Redux Toolkit - rtk query - getTodoById (2)

--- src/store/apis/todosApi.js
+++ src/store/apis/todosApi.js
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// Define a service using a base URL and expected endpoints
 export const todosApi = createApi({
   reducerPath: 'todosApi',
   baseQuery: fetchBaseQuery({
@@ -16,6 +15,4 @@ export const todosApi = createApi({
   }),
 })
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
 export const { useGetTodosQuery, useGetTodoQuery } = todosApi
